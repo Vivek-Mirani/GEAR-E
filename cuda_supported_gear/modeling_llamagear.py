79c7080ada413f082ed8ce793d895a84b32ceb97
Vivek Mirani <93434281+Vivek-Mirani@users.noreply.github.com>
2025-03-22 18:27:12 +0530

    Update modeling_llamagear.py for importing add_start_docstrings_to_model_forward

--- cuda_supported_gear/modeling_llamagear.py
+++ cuda_supported_gear/modeling_llamagear.py
@@ -12,6 +12,7 @@ from quant.matmul import cuda_bmm_fA_qB_outer
 from transformers.models.llama.configuration_llama import *
 from transformers.models.llama.modeling_llama import *
 from transformers.modeling_attn_mask_utils import _prepare_4d_causal_attention_mask
+from transformers.utils import add_start_docstrings_to_model_forward
 
 _CONFIG_FOR_DOC = "LlamaConfig"
 def if_not_then_None(variable):
