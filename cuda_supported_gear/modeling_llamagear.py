e98d2f6f15c966aa9dd9fb976529d0039e5aa087
GarimaBansal72 <103357469+GarimaBansal72@users.noreply.github.com>
2025-03-22 19:37:15 +0530

    Update modeling_llamagear.py

--- cuda_supported_gear/modeling_llamagear.py
+++ cuda_supported_gear/modeling_llamagear.py
@@ -339,6 +339,7 @@ class LlamaAttention_GEAR(nn.Module):
                 base=self.rope_theta,
             )
         else:
+            print(self.config.rope_scaling)
             scaling_type = self.config.rope_scaling["type"]
             scaling_factor = self.config.rope_scaling["factor"]
             if scaling_type == "linear":
