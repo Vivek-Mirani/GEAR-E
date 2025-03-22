cc3b1d118d0f6ddcd6b337884f6245e1da89eaa8
Vivek Mirani <93434281+Vivek-Mirani@users.noreply.github.com>
2025-03-22 15:40:12 +0530

    idk what is happening

--- GenerationBench/GenerationTest/GEARLM/Simulated/modeling_llama_new.py
+++ GenerationBench/GenerationTest/GEARLM/Simulated/modeling_llama_new.py
@@ -452,7 +452,7 @@ class LlamaAttention(nn.Module):
                 base=self.rope_theta,
             )
         else:
-            scaling_type = self.config.rope_scaling["type"]
+            scaling_type = self.config.rope_scaling["rope_type"]
             scaling_factor = self.config.rope_scaling["factor"]
             if scaling_type == "linear":
                 self.rotary_emb = LlamaLinearScalingRotaryEmbedding(
